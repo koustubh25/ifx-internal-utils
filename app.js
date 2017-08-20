4d9478d6d8eab9f3094f150bd8c420cf87020c87
koustubh <kosta250@gmail.com>
2017-08-20 11:08:32 +0900

    Extended functionalities

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-
-
-//Check cassandra connection
-let cass = require('./casssandra-migration/cassandra');
-let cassandra_bt = new cass([ process.env.CASSANDRA_BT_IP ], process.env.CASSANDRA_BT_KEYSPACE);
-cassandra_bt.checkConnection(function(result){
-  if(!result)
-    console.log('connecion unsccessful');
-  else{
-    console.log('connection succesful to ' + process.env.CASSANDRA_BT_IP);
-    require('./fetch-resources');
-  }
-
-});
-console.log('ed');
+//require('./casssandra-migration/execute_cql');
+require('./manage-projects/sync_source');
--- casssandra-migration/cassandra.js
+++ casssandra-migration/cassandra.js
@@ -7,20 +7,17 @@ class cassandra  {
         console.log(end_points);
         this.end_points = end_points;
         this.keyspace = keyspace;
+        this.client = this.getClient();
     }
 
-    checkConnection(callback) {
-
-        const client = new cassandra_driver.Client({
+    getClient(){
+        return new cassandra_driver.Client({
             contactPoints:  this.end_points ,
             keyspace: this.keyspace
         });
 
-        return client._connectCb(function(err){
-            callback(typeof err == 'undefined');
-        });
-
     }
+
 };
 
 module.exports =  cassandra;
